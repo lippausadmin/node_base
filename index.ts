f873073a403cdae989fb8d1210b9aceb4e55a029
lippausadmin <admin@lippausdistribuicao.com.br>
2022-08-09 08:38:04 -0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import passport from "passport";
 import cors from "cors"
 import { product } from "./src/controllers/Product";
 import * as UserControllers from "./src/controllers/User"
+import routes from "./src/routes/routes";
 // import { PrismaClient } from '@prisma/client'
 
 // const prisma = new PrismaClient()
@@ -17,7 +18,7 @@ server.use(cors())
 
 // server.use(passport.initialize())
 
-// server.use(routes)
+server.use(routes)
 
 // server.listen(3005);
 
@@ -29,12 +30,12 @@ server.use(cors())
 
 // USERS
 
-server.use("/product", product);
+// server.use("/product", product);
 
-server.use("/users", UserControllers.getUser)
-// routes.get("/user/:id", UserControllers.getUser)
-server.use("/user", UserControllers.createUser)
-server.use("/user", UserControllers.login)
+// server.use("/users", UserControllers.getUser)
+// // routes.get("/user/:id", UserControllers.getUser)
+// server.use("/user", UserControllers.createUser)
+// server.use("/user", UserControllers.login)
 // routes.put("/user/:id", UserControllers.updateUser)
 
 
