61f085dfd9a5a6976091f818590ad48dbcb64a10
lippausadmin <admin@lippausdistribuicao.com.br>
2023-01-10 10:28:02 -0300

    node base

--- index.ts
+++ index.ts
@@ -1,42 +1,13 @@
 import express from "express"
-import passport from "passport";
 import cors from "cors"
-import { product } from "./src/controllers/Product";
-import * as UserControllers from "./src/controllers/User"
 import routes from "./src/routes/routes";
-// import { PrismaClient } from '@prisma/client'
-
-// const prisma = new PrismaClient()
-// // use `prisma` in your application to read and write data in your DB
-
 
 const server = express();
 
+server.use(express.json());
 
-server.use(express.json())
-server.use(cors())
+server.use(cors());
 
-// server.use(passport.initialize())
-
-server.use(routes)
-
-// server.listen(3005);
-
-// GET
-// POST
-// PUT 
-// PATCH 
-// DELETE
-
-// USERS
-
-// server.use("/product", product);
-
-// server.use("/users", UserControllers.getUser)
-// // routes.get("/user/:id", UserControllers.getUser)
-// server.use("/user", UserControllers.createUser)
-// server.use("/user", UserControllers.login)
-// routes.put("/user/:id", UserControllers.updateUser)
-
+server.use(routes);
 
 server.listen(3005);
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,20 +1,7 @@
 import express from "express";
-import * as UserControllers from "../controllers/User"
 
 const routes = express.Router();
 
-// GET
-// POST
-// PUT 
-// PATCH 
-// DELETE
 
-// USERS
-
-routes.get("/users", UserControllers.getUser)
-// routes.get("/user/:id", UserControllers.getUser)
-routes.post("/user", UserControllers.createUser)
-routes.patch("/user", UserControllers.login)
-// routes.put("/user/:id", UserControllers.updateUser)
 
 export default routes;
